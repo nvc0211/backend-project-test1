d56075917f78ffdfb138cb74178ce2808ad0597c
NGUYEN VAN CUONG <nvcengineer@gmail.com>
2023-02-28 11:41:21 +0700

    Backend Template

---
+++ src/controllers/homeController.js
@@ -0,0 +1,13 @@
+
+let getHomePage = (req, res) => {
+    // return res.send("welcome to HomePage");
+    return res.render('homepage.ejs');
+}
+let getAboutme = (req, res) => {
+    return res.render('./test/about.ejs');
+}
+
+module.exports = {
+    getHomePage: getHomePage,
+    getAboutme: getAboutme
+}
